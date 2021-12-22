4fcb991a5b666e1962360274ca04f55df524c333
Alondrob <alondrob@gmail.com>
2021-12-21 21:28:26 -0500

    12.21.21

--- cinemacity-frontend/src/App.js
+++ cinemacity-frontend/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-import Movies from './movies/Data';
 import Navigator from './general/Navigator';
 import HomePage from './general/HomePage';
 import Menu from './general/Menu';
 import { BrowserRouter as Router, Routes, Route, Navigate, Link, Outlet, useParams, NavLink} from 'react-router-dom';
 import React from 'react';
 import MovieFinder from './movies/MovieFinder';
+import { movieData } from './utils/movies';
+
 
 const App = () => {
   return (
@@ -14,9 +15,10 @@ const App = () => {
 
       <Router>
         <Menu />
+        
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/moviefinder" element={<MovieFinder />} />
+          <Route path="/moviefinder" element={<MovieFinder movieData={movieData} placeholder="Search Movie...." />} />
         </Routes>
       </Router>
 
--- cinemacity-frontend/src/movies/MovieFinder.js
+++ cinemacity-frontend/src/movies/MovieFinder.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
 
-const MovieFinder= ({movieData}) => {
+
+const MovieFinder= ({movieData, placeholder}) => {
     console.log('hello')
-    const [searchMovies, setSearchMovies] = useState([])
-    const [searchedWords, setSearchedWords] = useState(" ")
+    const [filteredMovies, setFilteredMovies] = useState([])
+    
 
     // const handleFilter = (e) => {
     //     const searchedMovie = e.target.value;
@@ -12,13 +13,41 @@ const MovieFinder= ({movieData}) => {
     //     })
     //     setSearchMovies(newMovieArr)
     // }
-    console.log(movieData)
+    
     const handleChange = (event) => {
-        setSearchedWords(event.target.value)
+        const searchedWord = event.target.value
+        const newFilter = movieData.filter((value) => {
+            return value.title.toLowerCase().includes(searchedWord.toLowerCase());
+            
+        })
+        if (searchedWord === '') {
+            return setFilteredMovies([])
+        }
+        else {
+            return setFilteredMovies(newFilter)
+        }
     }
+
+   
+    
     return (
         <div>
-            <input type="text" placeholder='Search...' onChange={handleChange} value={searchedWords} />
+            <input 
+                type="text" 
+                placeholder={placeholder} 
+                onChange={handleChange} 
+                
+             />
+             {filteredMovies.length != 0 && (
+                 <div>
+                     {filteredMovies.map((value, key) => {
+                         return <a>{value.title}</a>
+                     })}
+                     </div>
+             )}
+             <div>
+                
+             </div>
         </div>
             
     )
